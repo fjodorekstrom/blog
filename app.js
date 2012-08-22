0da4674cdb6f58163b5b1d2dba490b0f1ab36234
fjodorekstromjayway <fjodor.ekstrom@jayway.com>
2012-08-22 10:48:05 +0200

    Fixed the articleprovider with dummyData

--- app.js
+++ app.js
@@ -6,8 +6,8 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
-
+  , path = require('path')
+  , ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 var app = express();
 
 app.configure(function(){
@@ -27,7 +27,14 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+var articleProvider = new ArticleProvider();
+
+//app.get('/', routes.index);
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
