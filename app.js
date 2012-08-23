715c4b885e52f70dcb373d6033c83fb59edbaaf7
fjodorekstromjayway <fjodor.ekstrom@jayway.com>
2012-08-23 19:52:57 +0200

    refactored back to not using route files and keeping routes in app.js

--- app.js
+++ app.js
@@ -29,15 +29,49 @@ app.configure('development', function(){
 
 var articleProvider = new ArticleProvider('alex@mongohq.com', 10034);
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', {
+            title: 'Blog',
+            articles:docs
+        });
+    })
+});
 
-app.get('/blog/new', routes.blog_new);
+app.get('/blog/new', function(req, res){
+  res.render('blog_new.jade', {
+    title: 'New Post'
+  });
+});
 
-app.post('/blog/new',  routes.blog_new_post);
+app.post('/blog/new', function(req, res){
+  articleProvider.save({
+    title: req.param('title'),
+    body: req.param('body')
+  }, function(error, docs){
+    res.redirect('/');
+  });
+});
 
-app.get('/blog/:id', routes.blog_show);
+app.get('/blog/:id', function(req, res){
+  articleProvider.findById(req.params.id, function(error, article){
+    res.render('blog_show.jade', {
+      title: article.title,
+      article:article
+    });
+  });
+});
 
-app.post('/blog/addComment', routes.add_comment);
+app.post('/blog/addComment', function(req, res){
+  articleProvider.addCommentToArticle(req.param('_id'), {
+    person: req.param('person'),
+    comment: req.param('comment'),
+    created_at: new Date()
+  }, function(error, docs){
+    res.redirect('/blog/' + req.param('_id'));
+  });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
